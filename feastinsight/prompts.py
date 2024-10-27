e0ce882cfa37cad472bb385ffe3792330af7d369
Sophie Cassidy <sophie.cassidy@ymail.com>
2024-10-27 17:51:37 +0000

    ChatGPT prompt to tag the sentiment & theme of restaurant reviews

--- feastinsight/prompts.py
+++ feastinsight/prompts.py
@@ -40,3 +40,19 @@ Now, create descriptive names for the following restaurants:
 # TODO: Get some real examples to input into the <example></example>
 # TODO: Get some examples of the types of titles GenAI creates and use this to come up with exclusions, inclusions - document and discuss this approach in the presentation
 #  
+
+THEMES = ["Service", "Ambiance", "Location", "Food Quality"] # TODO: Any other themes that would be useful?
+SENTIMENT = ["Positive", "Negative", "Neutral"]
+
+CHATGPT_USER_MESSAGE = (
+    f"""
+I am going to provide you with a list of reviews from customers of a restaurant.
+Your job is to tag each review with the following themes: {THEMES} and the following sentiment: {SENTIMENT}.
+You should also rate the review from 1 to 5 where 1 is the most negative and 5 is the most positive (excellent).
+Please only assign one theme for each piece of feedback.
+The keys in the dictionaires should be review, theme, sentiment, and rating.
+Please output results in a string formatted as a dictionary.
+Please only return one dictionary for the feedback.
+"""
+)
+# TODO: Is there any prompt engineering formats that work particularly well for Chat GPT?
