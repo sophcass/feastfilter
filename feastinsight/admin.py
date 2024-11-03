77f2f1bb8fbfbebca7dca2d22be806422e05757b
Sophie Cassidy <sophie.cassidy@ymail.com>
2024-11-03 12:36:18 +0000

    Add admin tables for models

--- feastinsight/admin.py
+++ feastinsight/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from feastinsight.models import Place, Review
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ["name", "formatted_address"]
+    search_fields = ["name"]
+    # TODO: Do we want to make some fields readonly?
+    # readonly_fields = (
+    #     "",
+    # )
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["place__name", "text"]
+    search_fields = ["place__name"]
+    # TODO: Do we want to make some fields readonly?
+    # readonly_fields = (
+    #     "",
+    # )
