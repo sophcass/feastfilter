3df579bc17e997983544643c69d1508ca1de7e40
Ratuja Reddy <ratuja@gmail.com>
2024-11-03 18:30:41 +0000

    call add sentimetn

--- feastinsight/management/commands/write_ai_insight.py
+++ feastinsight/management/commands/write_ai_insight.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 
+from feastinsight.ai_functions.openai import add_sentiment_of_review
+
 class Command(BaseCommand):
     help = "Populate insight of the restaurant using GenAI"
 
     def handle(self, *args, **kwargs):
-        pass
+        add_sentiment_of_review()
