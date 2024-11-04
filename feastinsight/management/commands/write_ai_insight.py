23fa0ecf3d899dc845e041a4cd27a4553eef702f
Ratuja Reddy <ratuja@gmail.com>
2024-11-04 18:17:49 +0000

    dont need write ai insight command anymore

--- feastinsight/management/commands/write_ai_insight.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Populate insight of the restaurant using GenAI"
-
-    def handle(self, *args, **kwargs):
-        pass
