7461b71594411bd3521178f3fcee8c8088710539
Ratuja Reddy <ratuja@gmail.com>
2024-11-10 16:36:00 +0000

    Add management commands for other functions

---
+++ feastinsight/management/commands/add_short_description.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+
+from feastinsight.models import Place
+from feastinsight.ai_functions.openai_functions import add_short_description
+
+class Command(BaseCommand):
+    help = "Add enhanced name to all restaurants"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--force',
+            action='store_true',
+            help='Force overwrite of existing enhanced name'
+        )
+
+    def handle(self, *args, **options):
+        restaurants = Place.objects.all()
+        force = options["force"]
+
+        for restaurant in restaurants.iterator():
+            if not restaurant.enhanced_name or force:
+                add_short_description(restaurant)
+            else:
+                print(
+                    f"Already populated enhanced_name for restaurant {restaurant.id}\n"
+                    "Use --force if you want to overwrite."
+                )
+
+        DJANGO_ADMIN_URL = "http://127.0.0.1:8000/admin/feastinsight/place/"
+        print(f"Finished writing enhanced name! Please visit {DJANGO_ADMIN_URL} to view results.")
