7d6116a663b36ebf50b161c6255b13ceb9aa184b
Ratuja Reddy <ratuja@gmail.com>
2024-11-11 20:00:34 +0000

    Update commands to add a param whether to use usage: openai [-h] [-v] [-b API_BASE] [-k API_KEY] [-p PROXY [PROXY ...]]               [-o ORGANIZATION] [-t {openai,azure}]               [--api-version API_VERSION] [--azure-endpoint AZURE_ENDPOINT]               [--azure-ad-token AZURE_AD_TOKEN] [-V]               {api,tools,migrate,grit} ...
    positional arguments:
      {api,tools,migrate,grit}
        api                 Direct API calls
        tools               Client side tools for convenience
    
    options:
      -h, --help            show this help message and exit
      -v, --verbose         Set verbosity.
      -b API_BASE, --api-base API_BASE
                            What API base url to use.
      -k API_KEY, --api-key API_KEY
                            What API key to use.
      -p PROXY [PROXY ...], --proxy PROXY [PROXY ...]
                            What proxy to use.
      -o ORGANIZATION, --organization ORGANIZATION
                            Which organization to run as (will use your default
                            organization if not specified)
      -t {openai,azure}, --api-type {openai,azure}
                            The backend API to call, must be `openai` or `azure`
      --api-version API_VERSION
                            The Azure API version, e.g.
                            'https://learn.microsoft.com/en-us/azure/ai-
                            services/openai/reference#rest-api-versioning'
      --azure-endpoint AZURE_ENDPOINT
                            The Azure endpoint, e.g.
                            'https://endpoint.openai.azure.com'
      --azure-ad-token AZURE_AD_TOKEN
                            A token from Azure Active Directory,
                            https://www.microsoft.com/en-
                            us/security/business/identity-access/microsoft-entra-
                            id
      -V, --version         show program's version number and exit or

--- feastinsight/management/commands/add_sentiment.py
+++ feastinsight/management/commands/add_sentiment.py
@@ -1,7 +1,7 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from feastinsight.models import Review
-from feastinsight.ai_functions.openai_functions import add_sentiment_of_review
+from feastinsight.ai_functions import openai_functions, anthropic_functions
 
 class Command(BaseCommand):
     help = "Add sentiment to all reviews"
@@ -12,10 +12,30 @@ class Command(BaseCommand):
             action='store_true',
             help='Force overwrite of existing sentiment'
         )
+        parser.add_argument(
+            "--open-ai",
+            action="store_true",
+            help="Use OpenAI function to get the enhanced name"
+        )
+        parser.add_argument(
+            "--claude",
+            action="store_true",
+            help="Use Claude function to get the enhanced name"
+        )
 
     def handle(self, *args, **options):
         reviews = Review.objects.all()
         force = options["force"]
+
+        if options["open_ai"] and options["claude"]:
+            raise CommandError("Cannot specify both --open-ai and --claude. Choose one.")
+        elif not options["open_ai"] and not options["claude"]:
+            raise CommandError("You must specify either --open-ai or --claude.")
+
+        if options["open_ai"]:
+            add_sentiment_of_review = openai_functions.add_sentiment_of_review
+        else:
+            add_sentiment_of_review = anthropic_functions.add_sentiment_of_review
 
         for review in reviews.iterator():
             if not review.sentiment or force:
