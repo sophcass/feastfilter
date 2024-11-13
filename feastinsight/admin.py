1715eb19e68d853946ff2a820dbd0f787fa3a800
Sophie Cassidy <sophie.cassidy@ymail.com>
2024-11-13 23:02:50 +0100

    Add additional fields to the admin dashboard view

--- feastinsight/admin.py
+++ feastinsight/admin.py
@@ -5,19 +5,11 @@ from feastinsight.models import Place, Review
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ["name", "formatted_address"]
+    list_display = ["name", "formatted_address", "openai_enhanced_name", "anthropic_enhanced_name"]
     search_fields = ["name"]
-    # TODO: Do we want to make some fields readonly?
-    # readonly_fields = (
-    #     "",
-    # )
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ["place__name", "text", "openai_sentiment", "anthropic_sentiment"]
     search_fields = ["place__name"]
-    # TODO: Do we want to make some fields readonly?
-    # readonly_fields = (
-    #     "",
-    # )
